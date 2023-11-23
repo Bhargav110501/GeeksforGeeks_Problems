ac98ba937aacd98fbc89ef981292fc3468273327
Bhargav Bandi <84260649+Bhargav110501@users.noreply.github.com>
2023-11-23 21:45:46 +0530

    Update bubble-sort.java

--- Easy/Bubble Sort/bubble-sort.java
+++ Easy/Bubble Sort/bubble-sort.java
@@ -16,12 +16,18 @@ class Solution
         //code here
         for(int i = n-1; i >= 1; i--){
             for(int j = 0; j <= i-1; j++){
+		    int didSwap = 0;
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+
+		    didSwap = 1;
                 }
             }
+		if(didSwap == 0){
+		    break;
+		}
         }
     }
 }
